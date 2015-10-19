d668383efbe7ac5d381f42a2cc95e05d08a8cd25
Jian Zhang <jzhang@membertek.com>
2015-10-18 22:08:01 -0400

    first commit

---
+++ app/src/main/java/coin/jianzhang/creditcards/ui/MainActivity.java
@@ -0,0 +1,100 @@
+package coin.jianzhang.creditcards.ui;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build.VERSION;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import coin.jianzhang.creditcards.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    final private int REQUEST_CODE_ASK_WRITE_PERMISSIONS = 111;
+    final private int REQUEST_CODE_ASK_READ_PERMISSIONS = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        //has to call the request permission for the API 23
+        requestReadExternalStoragePermission();
+        requestWriteExternalStoragePermission();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_READ_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission Granted
+                } else {
+                    // Permission Denied
+                    Toast.makeText(MainActivity.this, "You need to allow read access to Files",
+                            Toast.LENGTH_SHORT).show();
+                    requestReadExternalStoragePermission();
+                }
+                break;
+            case REQUEST_CODE_ASK_WRITE_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    Toast.makeText(MainActivity.this, "You need to allow write access to Files",
+                            Toast.LENGTH_SHORT).show();
+                    requestWriteExternalStoragePermission();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void requestReadExternalStoragePermission() {
+
+        if (VERSION.SDK_INT >= 23) {
+            int hasWriteContactsPermission = checkSelfPermission(Manifest.
+                    permission.READ_EXTERNAL_STORAGE);
+            if (hasWriteContactsPermission != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_CODE_ASK_READ_PERMISSIONS);
+            }
+        }
+    }
+
+    private void requestWriteExternalStoragePermission() {
+
+        if (VERSION.SDK_INT >= 23) {
+            int hasWriteContactsPermission = checkSelfPermission(Manifest.
+                    permission.WRITE_EXTERNAL_STORAGE);
+            if (hasWriteContactsPermission != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        REQUEST_CODE_ASK_WRITE_PERMISSIONS);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/coin/jianzhang/creditcards/utils/FileCache.java
@@ -0,0 +1,39 @@
+package coin.jianzhang.creditcards.utils;
+
+import android.content.Context;
+
+import java.io.File;
+import java.net.URLEncoder;
+
+/**
+ * Created by jianzhang on 10/17/15.
+ */
+public class FileCache {
+    private File cacheDir;
+
+    public FileCache(Context context) {
+        //Find the dir to save cached images
+        if (android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED))
+            cacheDir = new File(android.os.Environment.getExternalStorageDirectory(), "TTImages_cache");
+        else
+            cacheDir = context.getCacheDir();
+        if (!cacheDir.exists())
+            cacheDir.mkdirs();
+    }
+
+    public File getFile(String url) {
+        //String filename = String.valueOf(url.hashCode());
+        String filename = URLEncoder.encode(url);
+        File file = new File(cacheDir, filename);
+        return file;
+
+    }
+
+    public void clear() {
+        File[] files = cacheDir.listFiles();
+        if (files == null)
+            return;
+        for (File file : files)
+            file.delete();
+    }
+}
